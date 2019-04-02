5a78fbeb95b14054ee372b22f9b4333a7e93ec83
Elise Hinman <43010657+edhinman@users.noreply.github.com>
2019-04-02 17:12:09 -0600

    added in map, data table, selection

--- server.R
+++ server.R
@@ -10,33 +10,48 @@
 library(shiny)
 library(plyr)
 
+# Load prepped data
 prepped_dat <- read.csv("prepped_tox_conv.csv")
+prepped_dat$ActivityStartDate = as.Date(as.character(prepped_dat$ActivityStartDate),"%m/%d/%Y")
+
+# Determine the number of samples and parameters for each MLID
 uni_type_name = unique(prepped_dat[,c("IR_MLID","IR_MLNAME","AU_Type")])
 data_amount = unique(prepped_dat[,c("IR_MLID","ActivityStartDate","R3172ParameterName")])
-data_amount = plyr::ddply(data_amount, .(IR_MLID), summarize, NCount = length(R3172ParameterName))
+data_amount = plyr::ddply(data_amount, .(IR_MLID), summarize, NCount = length(R3172ParameterName), NParam = length(unique(R3172ParameterName)))
 
+# Load assessed data
 assessed_dat <- read.csv("assessed_tox_conv.csv")
+
+# Rank the categories for colors
 assessed_dat = within(assessed_dat,{
   Num_Cat = NA
-  Num_Cat[IR_Cat == "FS"] <- 1
-  Num_Cat[IR_Cat == "idE"] <- 2
-  Num_Cat[IR_Cat == "idNE"] <- 3
+  Num_Cat[IR_Cat == "idNE"] <- 1
+  Num_Cat[IR_Cat == "FS"] <- 2
+  Num_Cat[IR_Cat == "idE"] <- 3
   Num_Cat[IR_Cat == "TMDL"] <- 4
   Num_Cat[IR_Cat == "NS"] <- 5
 })
 unicats = unique(assessed_dat[,c("IR_MLID", "AU_NAME","R317Descrp","BEN_CLASS","IR_Lat","IR_Long","IR_Cat", "Num_Cat")])
 unicats1 = plyr::ddply(unicats, .(IR_MLID, AU_NAME, R317Descrp, BEN_CLASS, IR_Lat, IR_Long),summarize, Categories = paste(IR_Cat, collapse = ", "), MaxCat = max(Num_Cat))
 
+# Merge assessed data with Ncount and NParam
 siteinfo = merge(unicats1, uni_type_name, all.x = TRUE)
 siteinfo = merge(siteinfo, data_amount, all.x = TRUE)
 siteinfo$radius = log(siteinfo$NCount)*3
-# Site coordinates
+
+# Determine params causing NS
+siteinfo_ns = unique(assessed_dat[assessed_dat$Num_Cat==5,!names(assessed_dat)%in%c("BeneficialUse")])
+siteinfo_ns = plyr::ddply(siteinfo_ns, .(IR_MLID, AU_NAME, R317Descrp, BEN_CLASS, IR_Lat, IR_Long),summarize, Impaired_Params = paste(R3172ParameterName, collapse = ", "))
+
+siteinfo = merge(siteinfo, siteinfo_ns, all.x=TRUE)
+# Convert assessed data to geographic coords for mapping
 site_coords=sf::st_as_sf(siteinfo, coords=c("IR_Long","IR_Lat"), crs=4326, remove=F)
 
 # Data table and plotting cast
-vizdat <- unique(prepped_dat[,c("BeneficialUse","BEN_CLASS","IR_MLID", "R3172ParameterName","ActivityStartDate","IR_Unit","AssessmentType","CriterionType","NumericCriterion","CriterionLabel","IR_Value")])
+#vizdat <- unique(prepped_dat[,c("BeneficialUse","BEN_CLASS","IR_MLID", "R3172ParameterName","ActivityStartDate","IR_Unit","AssessmentType","CriterionType","NumericCriterion","CriterionLabel","IR_Value")])
+vizdat <- unique(prepped_dat[,c("BEN_CLASS","IR_MLID", "R3172ParameterName","ActivityStartDate","IR_Unit","IR_Value")])
 
-vizdat1 <- tidyr::spread(vizdat, BeneficialUse, NumericCriterion, fill= NA)
+#vizdat1 <- tidyr::spread(vizdat, BeneficialUse, NumericCriterion, fill= NA)
 
 ################ SERVER ##################
 
@@ -50,6 +65,8 @@ pal1 = leaflet::colorFactor(pal, domain=unicats1$MaxCat)
 # Empty reactive values object
 reactive_objects=reactiveValues()
 
+### SELECT DATA ###
+
 observe({
   req("map")
   reactive_objects$site_coords = site_coords
@@ -57,6 +74,9 @@ observe({
 
 # Map
 map = leaflet::createLeafletMap(session, 'map')
+
+# Map proxy
+map_proxy = leaflet::leafletProxy("map")
 # map
 session$onFlushed(once = T, function() {
   output$map <- leaflet::renderLeaflet({
@@ -67,11 +87,11 @@ session$onFlushed(once = T, function() {
                         "<br> AU ID: ", au_poly$ASSESS_ID,
                         "<br> AU type: ", au_poly$AU_Type,
                         "<br> Category: ", au_poly$au_colors))
-    map = addPolygons(map, data=wqTools::bu_poly,group="Beneficial uses",smoothFactor=3,opacity=0.9, ,fillOpacity = 0.1,weight=2,color="green", options = pathOptions(pane = "underlay_polygons"),
+    map = addPolygons(map, data=wqTools::bu_poly,group="Beneficial uses",smoothFactor=3,opacity=0.9,fillOpacity = 0.1,weight=2,color="green", options = pathOptions(pane = "underlay_polygons"),
                       popup=paste0(
                         "Description: ", bu_poly$R317Descrp,
                         "<br> Uses: ", bu_poly$bu_class))
-    map = addPolygons(map, data=wqTools::ss_poly,group="Site-specific standards",smoothFactor=3,opacity=0.9, ,fillOpacity = 0.1,weight=2,color="blue", options = pathOptions(pane = "underlay_polygons"),
+    map = addPolygons(map, data=wqTools::ss_poly,group="Site-specific standards",smoothFactor=3,opacity=0.9,fillOpacity = 0.1,weight=2,color="blue", options = pathOptions(pane = "underlay_polygons"),
                       popup=paste0("SS std: ", ss_poly$SiteSpecif))
     map = leaflet::addLayersControl(map,
                                     position ="topleft",
@@ -85,13 +105,101 @@ session$onFlushed(once = T, function() {
                              "<br> ML Name: ", reactive_objects$site_coords$IR_MLNAME,
                              "<br> Site Type: ", reactive_objects$site_coords$AU_Type,
                              "<br> Categories: ", reactive_objects$site_coords$Categories,
-                             "<br> Sample Count: ", as.character(reactive_objects$site_coords$NCount)))
+                             "<br> Impaired Params: ", reactive_objects$site_coords$Impaired_Params,
+                             "<br> Parameter Count: ", reactive_objects$site_coords$NParam,
+                             "<br> Sample Count: ", as.character(reactive_objects$site_coords$NCount),
+                             "<br></br>",
+                             actionButton("sel_site", "Select Site", onclick = 'Shiny.onInputChange(\"button_click\",  Math.random())')
+                             ))
     map=hideGroup(map, "Assessment units")
     map=hideGroup(map, "Site-specific standards")
     map=hideGroup(map, "Beneficial uses") 
-    
-  }) 
-})  
+    map=removeMeasure(map)
+    map=leaflet::addLegend(map, position = 'topright',
+                           colors = unique(pal1(reactive_objects$site_coords$MaxCat)), 
+                           labels = c("idE","FS","idNE","NS"))
+    #map=selectFeatures(reactive_objects$site_coords, mode = "click")
+  })
+})
 
-  
+siteinfo_abridged = siteinfo[,!names(siteinfo)%in%c("IR_Lat","IR_Long","R317Descrp","MaxCat","radius")]
+
+## SITE LIST TO SELECT FROM
+output$site_list <- DT::renderDT({
+  DT::datatable(
+    siteinfo_abridged, rownames=FALSE, filter="top",
+    options = list(scrollY = '300px', paging = FALSE, scrollX=TRUE, dom="ltipr")
+  )
+})
+outputOptions(output, "site_list", suspendWhenHidden = FALSE)
+
+# Register map clicks
+observeEvent(input$button_click,{
+  if(is.null(reactive_objects$sel_sites)){
+    reactive_objects$sel_sites = input$map_marker_click$id
+  }else{reactive_objects$sel_sites = unique(append(reactive_objects$sel_sites,input$map_marker_click$id))}
+map_proxy%>%removePopup(layerId = reactive_objects$site_coords$IR_MLID)
+})
+
+# Register row selections
+observeEvent(input$site_list_rows_selected,{
+  mlids = as.character(siteinfo_abridged[c(input$site_list_rows_selected),"IR_MLID"])
+  if(is.null(reactive_objects$sel_sites)){
+    reactive_objects$sel_sites = mlids
+  }else{reactive_objects$sel_sites = unique(append(reactive_objects$sel_sites,mlids))}
+})
+
+# Populate selected site list
+observe({
+  if(!is.null(reactive_objects$sel_sites)){
+    selsites = siteinfo_abridged[siteinfo_abridged$IR_MLID%in%reactive_objects$sel_sites,c("IR_MLID","IR_MLNAME")]
+  }else{
+    selsites = siteinfo_abridged[0,c("IR_MLID","IR_MLNAME")]
+  }
+  output$selsite_list <- DT::renderDT({
+    DT::datatable(
+      selsites, rownames=FALSE,
+      options = list(scrollY = '200px', paging = FALSE, dom="t")
+    )
+  })
+  outputOptions(output, "selsite_list", suspendWhenHidden = FALSE)
+})
+
+# Clear table filter buttons
+observeEvent(input$clear_sitelist, ignoreInit=T,{
+  reactive_objects$sel_sites = NULL
+})
+
+##### FILTER DATA ####
+
+observe({
+  reactive_objects$filtered_data = prepped_dat[prepped_dat$IR_MLID%in%reactive_objects$sel_sites,]
+  reactive_objects$params = unique(reactive_objects$filtered_data$R3172ParameterName)
+  if(!is.null(input$sel_params)){
+    updatedata <- isolate(reactive_objects$filtered_data)
+    reactive_objects$filtered_data = updatedata[updatedata$R3172ParameterName%in%reactive_objects$params,]
+    reactive_objects$uses = unique(reactive_objects$filtered_data$BeneficialUse)}
+  if(!is.null(input$sel_use)){
+    updatedata <- isolate(reactive_objects$filtered_data)
+    print(min(updatedata$ActivityStartDate))
+    reactive_objects$filtered_data = updatedata[updatedata$BeneficialUse%in%reactive_objects$uses,]
+    reactive_objects$maxdate = max(updatedata$filtered_data$ActivityStartDate)
+    reactive_objects$mindate = min(updatedata$filtered_data$ActivityStartDate)}
+})
+
+output$sel_params <- renderUI({
+  selectizeInput("sel_params","Select R3172 Parameter(s)",choices = reactive_objects$params, multiple = TRUE, selected = NULL)
+})
+
+output$sel_use <- renderUI({
+  selectizeInput("sel_use", "Select Uses", choices = reactive_objects$uses, multiple = TRUE, selected = NULL)
+})
+
+output$sel_date <- renderUI({
+  req(reactive_objects$maxdate)
+  sliderInput("sel_date", "Select Date Range", value = c(reactive_objects$mindate,reactive_objects$maxdate), min = reactive_objects$mindate, max = reactive_objects$maxdate)
+})
+
+
+
 })
--- ui.R
+++ ui.R
@@ -31,13 +31,39 @@ shinyUI(fluidPage(
   #                selectInput("cattype", "Site Category", c("Not Supporting", "Insufficient Data - Exceedances","Insufficient Data - No Exceedances", "TMDL Approved", "No Evidence of Impairment","Supporting")),
   #                actionButton("reset_filter", "Reset Filter")),
     fluidRow(
-      bsCollapse(id = "collpanels", multiple = TRUE, open = "Map",
-                bsCollapsePanel("Map",
-                               fluidRow(column(6,actionButton("reset_zoom", label = "Reset map zoom",style='color: #fff; background-color: #337ab7; border-color: #2e6da4;font-size:120%'))),
-                               br(),      
-                               fluidRow(shinycssloaders::withSpinner(leaflet::leafletOutput("map", height="600px"),size=2, color="#0080b7"))),
-                bsCollapsePanel("Data"),
-                bsCollapsePanel("Visualizations"))
+      useShinyjs(),
+      br(),
+      h4("Expand and collapse Select Data, Filter Data, and View Data panes by clicking the pane title."),
+      bsCollapse(id = "collpanels", multiple = TRUE,
+                bsCollapsePanel("Select Data",
+                                bsCollapse(id = "seldatpanels", multiple=TRUE,
+                                bsCollapsePanel("Map",
+                                                strong("Click site markers to view summary data. Click 'Select Site' button to add site to list below to view data."),
+                                         # fluidRow(column(6,actionButton("reset_zoom", label = "Reset map zoom",style='color: #fff; background-color: #337ab7; border-color: #2e6da4;font-size:120%'))),
+                                         # br(),      
+                                         fluidRow(shinycssloaders::withSpinner(leaflet::leafletOutput("map", height="600px"),size=2, color="#0080b7"))),
+                                bsCollapsePanel("Data",
+                                                fluidRow(strong("Select site(s) to analyze in tables and plots below."),
+                                                         div(DT::DTOutput("site_list"), style = "font-size:70%"))
+                                ),
+                                bsCollapsePanel("Selected Site(s)",
+                                                fluidRow(strong("Selected site(s) from map and table will appear here.")),
+                                                br(),
+                                                fluidRow(actionButton("clear_sitelist","Clear Site List",style='color: #fff; background-color: #337ab7; border-color: #2e6da4;font-size:120%')),
+                                                br(),
+                                                fluidRow(div(DT::DTOutput("selsite_list"), style = "font-size:70%"))
+                                                )
+                                )),
+                bsCollapsePanel("Filter Data",
+                                h4("Further drill down data specifics in this tab before exploring plots, below"),
+                                br(),
+                                br(),
+                                fluidRow(column(4,uiOutput("sel_params")),
+                                         column(4, uiOutput("sel_use")),
+                                         column(4,uiOutput("sel_date"))),
+                                br(),
+                                fluidRow(div(DT::DTOutput("filtered_data"), style = "font-size:70%"))
+                                ),
+                bsCollapsePanel("View Data"))
   )
-  # )
   ))
